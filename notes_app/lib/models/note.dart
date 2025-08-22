f63edb6d7a208a1f8ca54eaf5de4093e9cd3568b
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-08-22 23:09:42 +0530

    Create  Note model and Create mock notes data

---
+++ notes_app/lib/models/note.dart
@@ -0,0 +1,7 @@
+class Note {
+  String title;
+  String content;
+  bool isPinned;
+
+  Note({required this.title, required this.content, this.isPinned = false});
+}
---
+++ notes_app/lib/utils/mock_data.dart
@@ -0,0 +1,8 @@
+import '../models/note.dart';
+
+List<Note> mockNotes = [
+  Note(title: "Shopping List", content: "Milk, Eggs, Bread", isPinned: true),
+  Note(title: "Meeting Notes", content: "Discuss project deadlines"),
+  Note(title: "Flutter Tips", content: "Use Provider for state management"),
+  Note(title: "Travel Plan", content: "Visit Colombo and Galle"),
+];
